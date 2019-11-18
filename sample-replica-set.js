7e74ac392e336cda4a83c9b4cd3bf83e636556f4
A. Jesse Jiryu Davis <jesse@mongodb.com>
2019-11-17 21:38:56 -0500

    Update sample-replica-set.js

--- sample-replica-set.js
+++ sample-replica-set.js
@@ -1,10 +1,27 @@
 const rst = new ReplSetTest({
-  nodes: 3,
-  nodeOptions: {
-    useLogFiles: true,
-    setParameter: {logComponentVerbosity: tojsononeline({tlaPlusTrace: 1})}
-  }
+    nodes: 3,
+    oplogSize: 999999,  // We don't model truncation in our specs, so disable it
+    nodeOptions: {
+        useLogFiles: true,
+        setParameter: {
+            "failpoint.logForTLAPlusSpecs":
+                tojson({mode: "alwaysOn", data: {specs: ["RaftMongo"]}}),
+            logComponentVerbosity: tojsononeline({tlaPlusTrace: 2}),
+        }
+    }
 });
+
 rst.startSet();
-rst.initiate();
-rst.getPrimary();
+// Skip ReplSetTest's usual logic of initiating a 1-node set and adding the others: we want as
+// simple a startup sequence as possible.
+const config = rst.getReplSetConfig();
+jsTestLog(`reconfig: ${tojson(config)}`);
+assert.commandWorked(rst.nodes[0].getDB('admin').runCommand({replSetInitiate: config}));
+const collection = rst.getPrimary().getDB('test').collection;
+const wc = {
+    writeConcern: {w: 'majority', wtimeout: 10000}
+};
+jsTestLog("single insert");
+printjson(assert.commandWorked(collection.insert({_id: 0}, wc)));
+jsTestLog("bulk insert");
+printjson(assert.commandWorked(collection.insertMany([{_id: 1}, {_id: 2}], wc)));
